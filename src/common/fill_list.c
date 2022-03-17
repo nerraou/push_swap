7a2b909c2ff184d4e8c355b90ca0761173cbbbbf
Nouhayla Erraou <nerraou@e1r8p5.1337.ma>
2022-03-17 18:11:39 +0100

    fix norm

--- src/common/fill_list.c
+++ src/common/fill_list.c
@@ -6,17 +6,16 @@
 /*   By: nerraou <nerraou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 17:17:00 by nerraou           #+#    #+#             */
-/*   Updated: 2022/03/17 17:21:51 by nerraou          ###   ########.fr       */
+/*   Updated: 2022/03/17 18:11:14 by nerraou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "common.h"
 
-static void	free_and_exit(t_list *list_a, t_list *list_b,  char **numbers)
+static void	free_and_exit(t_list *list_a, t_list *list_b, char **numbers)
 {
-
-		free_2d(numbers);
-		free_exit(&list_a, &list_b, 1, "Error\n");
+	free_2d(numbers);
+	free_exit(&list_a, &list_b, 1, "Error\n");
 }
 
 void	fill_list(t_list *list_a, t_list *list_b, int ac, char *av[])
